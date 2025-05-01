69ff8d1cbcbedcd8150dc1bb0ef2debbc8996d5a
Zeutyz <gustavoreduaoliveira@gmail.com>
2025-04-30 22:29:34 -0300

    feat: Implementa conteúdo introdutório (Classes 1-3)

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import '@/app/ui/global.css';
+import { lusitana } from '@/app/ui/fonts';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
